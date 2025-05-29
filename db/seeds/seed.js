587939af473028ecf5849b0cd4ad8dab62c0ecd6
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-05-29 16:00:50 +0100

    created users table and passed all tests for users table

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,12 +1,21 @@
 const db = require("../connection");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(`DROP TABLE IF EXISTS topics;`).then(() => {
-    return db.query(`CREATE TABLE topics (
+  return db
+    .query(`DROP TABLE IF EXISTS users,topics;`)
+    .then(() => {
+      return db.query(`CREATE TABLE topics (
     slug VARCHAR(50) PRIMARY KEY,
     description VARCHAR(255) NOT NULL,
     img_url VARCHAR(1000)
     );`);
-  });
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    avatar_url VARCHAR(1000)
+    );`);
+    });
 };
 module.exports = seed;
--- tables.sql
+++ tables.sql
@@ -1,7 +1,13 @@
-DROP TABLE IF EXISTS topics;
+DROP TABLE IF EXISTS users, topics;
 
 CREATE TABLE topics (
     slug VARCHAR(50) PRIMARY KEY,
     description VARCHAR(255) NOT NULL,
     img_url VARCHAR(1000)
-)
+);
+
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    avatar_url VARCHAR(1000)
+);
