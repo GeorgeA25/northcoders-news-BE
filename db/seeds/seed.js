c1dfdd8d70d7d00612311128e1ae8ed9b94a3cb8
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-05-29 15:19:36 +0100

    refactored seed.js file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,11 +1,12 @@
 const db = require("../connection");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(`DROP TABLE IF EXISTS topics;
-    CREATE TABLE topics (
+  return db.query(`DROP TABLE IF EXISTS topics;`).then(() => {
+    return db.query(`CREATE TABLE topics (
     slug VARCHAR(50) PRIMARY KEY,
     description VARCHAR(255) NOT NULL,
     img_url VARCHAR(1000)
-);`);
+    );`);
+  });
 };
 module.exports = seed;
