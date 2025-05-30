d25f93b48fd617652665789c1447713baa23ae21
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-05-30 16:57:47 +0100

    inserted comments data and passed the test for inserted comments data correctly and then refactored seed.js file to incorperate async/await

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,25 +2,19 @@ const db = require("../connection");
 const format = require("pg-format");
 const { convertTimestampToDate, createLookupObj } = require("./utils");
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db
-    .query(`DROP TABLE IF EXISTS comments, articles, users, topics;`)
-    .then(() => {
-      return db.query(`CREATE TABLE topics (
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+  await db.query(`DROP TABLE IF EXISTS comments, articles, users, topics;`);
+  await db.query(`CREATE TABLE topics (
         slug VARCHAR(50) PRIMARY KEY,
         description VARCHAR(255) NOT NULL,
         img_url VARCHAR(1000)
     );`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE users (
+  await db.query(`CREATE TABLE users (
         username VARCHAR(50) PRIMARY KEY,
         name VARCHAR(200) NOT NULL,
         avatar_url VARCHAR(1000)
     );`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE articles (
+  await db.query(`CREATE TABLE articles (
         article_id SERIAL PRIMARY KEY,
         title VARCHAR(255),
         topic VARCHAR(50) REFERENCES topics(slug),
@@ -30,9 +24,7 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
         votes INT DEFAULT 0,
         article_img_url VARCHAR(1000)
     );`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE comments (
+  await db.query(`CREATE TABLE comments (
         comment_id SERIAL PRIMARY KEY,
         article_id INT REFERENCES articles(article_id),
         body TEXT NOT NULL,
@@ -40,54 +32,68 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
         author VARCHAR(50) REFERENCES users(username),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`);
-    })
-    .then(() => {
-      const topicInsertQuery = format(
-        `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *;`,
-        topicData.map(({ slug, description, img_url }) => [
-          slug,
-          description,
-          img_url,
-        ])
-      );
-      return db.query(topicInsertQuery);
-    })
-    .then(() => {
-      const userInsertData = format(
-        `INSERT INTO users (username, name, avatar_url) VALUES %L RETURNING *;`,
-        userData.map(({ username, name, avatar_url }) => [
-          username,
-          name,
-          avatar_url,
-        ])
-      );
-      return db.query(userInsertData);
-    })
-    .then(() => {
-      const formattedArticles = articleData.map(convertTimestampToDate);
-      const articlesInsertData = format(
-        `INSERT INTO articles(title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *;`,
-        formattedArticles.map(
-          ({
-            title,
-            topic,
-            author,
-            body,
-            created_at,
-            votes,
-            article_img_url,
-          }) => [
-            title,
-            topic,
-            author,
-            body,
-            new Date(created_at),
-            votes,
-            article_img_url,
-          ]
-        )
-      );
-      return db.query(articlesInsertData);
-    });
+
+  const topicInsertQuery = format(
+    `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *;`,
+    topicData.map(({ slug, description, img_url }) => [
+      slug,
+      description,
+      img_url,
+    ])
+  );
+  await db.query(topicInsertQuery);
+
+  const userInsertData = format(
+    `INSERT INTO users (username, name, avatar_url) VALUES %L RETURNING *;`,
+    userData.map(({ username, name, avatar_url }) => [
+      username,
+      name,
+      avatar_url,
+    ])
+  );
+  await db.query(userInsertData);
+
+  const formattedArticles = articleData.map(convertTimestampToDate);
+  const articlesInsertData = format(
+    `INSERT INTO articles(title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *;`,
+    formattedArticles.map(
+      ({ title, topic, author, body, created_at, votes, article_img_url }) => [
+        title,
+        topic,
+        author,
+        body,
+        created_at,
+        votes,
+        article_img_url,
+      ]
+    )
+  );
+
+  const { rows: insertedArticles } = await db.query(articlesInsertData);
+  const articleLookup = createLookupObj(
+    insertedArticles,
+    "title",
+    "article_id"
+  );
+
+  const commentsWithIdAndDates = commentData.map((comment) => ({
+    ...comment,
+    article_id: articleLookup[comment.article_title],
+    created_at: convertTimestampToDate(comment).created_at,
+  }));
+
+  const commentInsertQuery = format(
+    `INSERT INTO comments (article_id, body, votes, author, created_at) VALUES %L RETURNING *;`,
+    commentsWithIdAndDates.map(
+      ({ article_id, body, votes, author, created_at }) => [
+        article_id,
+        body,
+        votes,
+        author,
+        created_at,
+      ]
+    )
+  );
+  return await db.query(commentInsertQuery);
 };
 module.exports = seed;
