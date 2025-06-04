d939e7699122a56e880c816d3a477e33e9b52ade
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-04 14:27:45 +0100

    Create test for GET /api/topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,12 +13,23 @@ afterAll(() => {
   return db.end();
 });
 describe.skip("GET /api", () => {
-  test("200: Responds with an object detailing the documentation for each endpoint", () => {
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
-      });
+  test("200: Responds with an object detailing the documentation for each endpoint", async () => {
+    const {
+      body: { endpoints },
+    } = await request(app).get("/api").expect(200);
+    expect(endpoints).toEqual(endpointsJson);
+  });
+});
+
+describe.skip("GET /api/topics", () => {
+  test("GET /api/topics responds with status 200 and returns an object containing a 'topics' key, where the value is an array of topic objects each having 'slug' and 'description' properties", async () => {
+    const {
+      body: { topics },
+    } = await request(app).get("/api/topics").expect(200);
+    expect(Array.isArray(topics)).toBe(true);
+    for (const topic of topics) {
+      expect(typeof topic.slug).toBe("string");
+      expect(typeof topic.description).toBe("string");
+    }
   });
 });
