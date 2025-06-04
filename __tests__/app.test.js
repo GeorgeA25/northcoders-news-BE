5631cea832edd071ba3b5fd8260e86b2972e19b4
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-04 13:48:33 +0100

    Update tests for API endpoint and database seeding

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,19 @@
+const request = require("supertest");
+const app = require("../app");
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const db = require("../db/connection");
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+  return seed(data);
+});
 
+afterAll(() => {
+  return db.end();
+});
 describe.skip("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
