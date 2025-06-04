fdcb470676452e2583325bca822b82249250342c
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-04 15:50:01 +0100

    Add GET /api/topics route to app.js and test passes and responds with a 200

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const { getApiDocs } = require("./controllers/api.controller");
+const { getTopics } = require("./controllers/topics.controller");
 
 app.get("/api", getApiDocs);
+
+app.get("/api/topics", getTopics);
 
 module.exports = app;
