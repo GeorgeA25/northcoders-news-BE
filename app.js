75d82fac981a1773b2ecd6cf320ff2432d2a2a0c
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-05 11:41:25 +0100

    Update app.js to import errorHandlers from errorHandlers.js and apply middleware using app.use

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const { getApiDocs } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
 const { getArticles } = require("./controllers/articles.controller");
+const { handle404, handle400, handle500 } = require("./errorHandlers");
 
 app.get("/api", getApiDocs);
 
@@ -10,4 +11,9 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getArticles);
 
+app.use(handle404);
+
+app.use(handle400);
+
+app.use(handle500);
 module.exports = app;
