df695c69da95c68c3b73106a089ced10fe4bf9d9
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-05 12:06:00 +0100

    Refactored getTopics function to include error handling with try-catch

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,8 +1,11 @@
 const { selectTopics } = require("../models/topics.model");
-
-const getTopics = async (request, response) => {
-  const topics = await selectTopics();
-  response.status(200).send({ topics });
+const getTopics = async (request, response, next) => {
+  try {
+    const topics = await selectTopics();
+    response.status(200).send({ topics });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { getTopics };
