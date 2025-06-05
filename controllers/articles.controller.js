7420f6363f55c2542c02bf59ea24e3aa76b64613
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-05 12:06:49 +0100

    Refactored getArticles function to include error handling with try-catch

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,8 +1,11 @@
 const { selectArticles } = require("../models/articles.model");
-
-const getArticles = async (request, response) => {
-  const articles = await selectArticles();
-  response.status(200).send({ articles });
+const getArticles = async (request, response, next) => {
+  try {
+    const articles = await selectArticles();
+    response.status(200).send({ articles });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { getArticles };
