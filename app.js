a7d29bd49c9c534079aa4842262cf307cecae2c5
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-06 13:23:05 +0100

    Add GET /api/articles/article_id and import getArticleById controller  and passes relevent tests. Add app.use(customHandlerError) to implement customeHandlerError middleware

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const { getApiDocs } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticles } = require("./controllers/articles.controller");
+const {
+  getArticles,
+  getArticlesById,
+} = require("./controllers/articles.controller");
 const { getUsers } = require("./controllers/users.controller");
-const { handle404, handle400, handle500 } = require("./errorHandlers");
+const {
+  handle404,
+  handle400,
+  customHandlerError,
+  handle500,
+} = require("./errorHandlers");
 
 app.get("/api", getApiDocs);
 
@@ -14,9 +22,13 @@ app.get("/api/articles", getArticles);
 
 app.get("/api/users", getUsers);
 
+app.get("/api/articles/:article_id", getArticlesById);
+
 app.use(handle404);
 
 app.use(handle400);
+
+app.use(customHandlerError);
 
 app.use(handle500);
 module.exports = app;
