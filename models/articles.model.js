8a07a527eb3997c294e87bd3b516647f5cf2bac4
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-06 14:31:11 +0100

    Add selectCommentsByArticleId function that queries comments for a given article_id

--- models/articles.model.js
+++ models/articles.model.js
@@ -30,4 +30,16 @@ const selectArticlesById = async (id) => {
   }
 };
 
-module.exports = { selectArticles, selectArticlesById };
+const selectCommentsByArticleId = async (article_id) => {
+  const { rows } = await db.query(
+    `SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body, comments.article_id FROM comments LEFT JOIN articles On comments.article_id = articles.article_id WHERE comments.article_id = $1 ORDER BY comments.created_at DESC;`,
+    [article_id]
+  );
+  return rows;
+};
+
+module.exports = {
+  selectArticles,
+  selectArticlesById,
+  selectCommentsByArticleId,
+};
