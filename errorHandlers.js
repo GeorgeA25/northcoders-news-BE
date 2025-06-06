709df52b3b44c0dadcd7dc3c1a16fade6306ca89
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-06 12:30:17 +0100

    Add customHandlerError middleware for handling custom status and message error

--- errorHandlers.js
+++ errorHandlers.js
@@ -1,17 +1,24 @@
-const handle404 = (request, response, next) => {
+const handle404 = (request, response, next) =>
   response.status(404).send({ message: "Route not found" });
-};
 
 const handle400 = (error, request, response, next) => {
   if (error.code === "22P02") {
-    response.status(400).send({ message: "bad request" });
+    response.status(400).send({ message: "Bad request" });
   } else {
     next(error);
   }
 };
 
+const customerHandlerError = (error, request, response, next) => {
+  if (error.status && error.message) {
+    return response.status(error.status).send({ message: error.message });
+  } else {
+    next(error);
+  }
+};
 const handle500 = (error, request, response, next) => {
+  console.log(error);
   response.status(500).send({ message: "Internal server error" });
 };
 
-module.exports = { handle404, handle400, handle500 };
+module.exports = { handle404, handle400, customerHandlerError, handle500 };
