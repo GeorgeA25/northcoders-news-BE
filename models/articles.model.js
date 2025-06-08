024ad743ee7890087f4a1be5789aaaf905a6f555
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-06-08 15:12:43 +0100

    Refactored selectArticles function to remove ::INT because the conversion from strings to numbers is handled by utils function

--- models/articles.model.js
+++ models/articles.model.js
@@ -10,10 +10,15 @@ const selectArticles = async () => {
         articles.created_at,
         articles.votes,
         articles.article_img_url,
-        COUNT(comments.comment_id)::INT AS comment_count
+        COUNT(comments.comment_id) AS comment_count
         FROM articles
         LEFT JOIN comments ON comments.article_id = articles.article_id
-        GROUP BY articles.article_id
+        GROUP BY articles.article_id, articles.title,
+        articles.topic,
+        articles.author,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url 
         ORDER BY articles.created_at DESC;`);
   return articles;
 };
