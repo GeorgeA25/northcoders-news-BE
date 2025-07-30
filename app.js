3bd44205fb6d15af88a15310c6a37848e56efa67
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-07-30 17:51:46 +0100

    Add routes for emojis and emoji_reactions

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ const {
   handle500,
 } = require("./errorHandlers");
 const cors = require("cors");
+const getEmojis = require("./controllers/emojis.controller");
+const {
+  getEmojiReactionsByArticleId,
+  createEmojiReaction,
+  removeEmojiReaction,
+} = require("./controllers/emojiReactions.controller");
 
 app.use(cors());
 
@@ -31,15 +37,26 @@ app.get("/api/articles", getArticles);
 
 app.get("/api/users", getUsers);
 
+app.get("/api/emojis", getEmojis);
+
+app.get("/api/emoji-reactions/:article_id", getEmojiReactionsByArticleId);
+
 app.get("/api/articles/:article_id", getArticlesById);
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
 app.post("/api/articles/:article_id/comments", postCommentsByArticleId);
 
+app.post("/api/emoji-reactions", createEmojiReaction);
+
 app.patch("/api/articles/:article_id", updateArticle);
 
 app.delete("/api/comments/:comment_id", deleteComment);
+
+app.delete(
+  "/api/emoji-reactions/:emoji_id/:username/:article_id",
+  removeEmojiReaction
+);
 
 app.use(handle404);
 
